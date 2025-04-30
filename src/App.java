63cd45e73df7e933d2a24bd88e9dd8520be0476a
felipe soares siqueira <felipesoaressiqueira@gmail.com>
2025-04-30 11:52:05 -0300

    aula 2 finalizada

--- src/App.java
+++ src/App.java
@@ -1,5 +1,54 @@
+//import javax.swing.JOptionPane;
+
+
+/*public class App {
+    public static void main(String[] args) throws Exception {
+        //termário
+        int op = Integer.parseInt(JOptionPane.showInputDialog("Qual o resultado de uma população? 1-Parâmetro 2-Estatística"));
+        if (op ==1 ) {
+            JOptionPane.showMessageDialog(null,"Resposta Certa ");
+                  
+            
+        }else{
+            JOptionPane.showMessageDialog(null, "Resposta errada");
+        }
+       
+    }
+}
+
+
+//if else 
+//exercícios
+//*Considerando os números 30,50,,80,,90 Digite o valor da média aritmética simples? */
+//gabarito: 62,5
+//*Considerando os valores 10,40,50,80,10. Digite qual é a moda?*/
+//10
+//*Qual a medida de tendência central que mostra o centro de amostra  */
+//mediana 
+
+//String nome;
+//if nome.equals ("mediana")
+
+//double resposta = Double.parseDouble()
+//10 min - 
+//30 minutos 
+//10:42 --- 11:12
+//
+
+import javax.swing.JOptionPane;
+
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Alo Git");
+        //termário
+        Double op = Double.parseDouble(JOptionPane.showInputDialog("Considerando os números 30,50,,80,,90 Digite o valor da média aritmética simples?"));
+        if (op ==62.5 ) {
+            JOptionPane.showMessageDialog(null,"Resposta Certa ");
+                  
+            
+        }else{
+            JOptionPane.showMessageDialog(null, "Resposta errada");
+        }
+       
     }
 }
