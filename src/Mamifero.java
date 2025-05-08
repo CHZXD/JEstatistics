3bd8f55096e1db1c7116ba00215f3326a9165ada
felipe soares siqueira <felipesoaressiqueira@gmail.com>
2025-05-08 11:38:34 -0300

    aula 6 finalizada

---
+++ src/Mamifero.java
@@ -0,0 +1,17 @@
+﻿public class Mamifero {
+    //atributos 
+    private int id;
+    private String nomeCientifico;
+
+    public int getId(){
+
+    }
+    public void setNomeCientifico(String nomeCientifico){
+        this.nomeCientifico = nomeCientifico;
+
+    }
+    public String getnomeCientifico(){
+        return this.nomeCientifico;
+    }
+
+}
